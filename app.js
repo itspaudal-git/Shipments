97b83a3f97c949479f6a6ac009caccfc17dc0626
itspaudal-git <80719235+itspaudal-git@users.noreply.github.com>
2023-03-06 12:40:01 -0600

    majoy change

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue, get } from 'firebase/database';
+
+// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBvjBF8oP9hhaorrsLU8pr2prCAmS7eXmg",
-    authDomain: "shipments-169d4.firebaseapp.com",
-    databaseURL: "https://shipments-169d4-default-rtdb.firebaseio.com",
-    projectId: "shipments-169d4",
-    storageBucket: "shipments-169d4.appspot.com",
-    messagingSenderId: "500866362157",
-    appId: "1:500866362157:web:9a4ddf3a59173bfc7041a4",
-    measurementId: "G-Y3QBFBL0LB"
+	apiKey: "AIzaSyBvjBF8oP9hhaorrsLU8pr2prCAmS7eXmg",
+	authDomain: "shipments-169d4.firebaseapp.com",
+	databaseURL: "https://shipments-169d4-default-rtdb.firebaseio.com",
+	projectId: "shipments-169d4",
+	storageBucket: "shipments-169d4.appspot.com",
+	messagingSenderId: "500866362157",
+	appId: "1:500866362157:web:9a4ddf3a59173bfc7041a4",
+	measurementId: "G-Y3QBFBL0LB"
   };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-// Get a reference to the "Shipments/Data" node in the Firebase database
-const expectedRef = firebase.database().ref('Shipments/Data');
+// Get a list of expected barcodes from your database
+async function getExpectedBarcodes(db) {
+  const expectedRef = ref(db, 'Shipments/Data');
+  const expectedSnapshot = await get(expectedRef);
+  const expectedList = Object.keys(expectedSnapshot.val() || {});
+  return expectedList;
+}
 
-// Get a reference to the "Shipments/Scanned" node in the Firebase database
-const scannedRef = firebase.database().ref('Shipments/Scanned');
+// Get the number of scanned barcodes from your database
+function getScannedCount(db) {
+  const scannedRef = ref(db, 'Shipments/Scanned');
+  onValue(scannedRef, (snapshot) => {
+    const scannedCount = snapshot.numChildren();
+    document.getElementById('scanned').innerText = scannedCount;
+  });
+}
 
 // Update the "Total" label with the total number of expected barcodes
-expectedRef.once('value').then(snapshot => {
-  const total = snapshot.numChildren();
+getExpectedBarcodes(db).then((expectedList) => {
+  const total = expectedList.length;
   document.getElementById('total').innerText = total;
 });
 
 // Update the "Scanned" label with the number of scanned barcodes
-scannedRef.on('value', snapshot => {
-  const scanned = snapshot.numChildren();
-  document.getElementById('scanned').innerText = scanned;
-});
+getScannedCount(db);
