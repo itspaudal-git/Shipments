e2979b1c1f6e5ae6082f28697f92aa217f645eed
itspaudal-git <80719235+itspaudal-git@users.noreply.github.com>
2023-03-06 12:00:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -15,32 +15,17 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the "Shipments/Data" node in the Firebase database
 const expectedRef = firebase.database().ref('Shipments/Data');
 
-// Set up an event listener for the "View Shipment Data" button
-const shipmentDataButton = document.getElementById('shipment-data-button');
-shipmentDataButton.addEventListener('click', () => {
-	// Retrieve the data from the Firebase database
-	expectedRef.once('value').then(snapshot => {
-		// Convert the data to a JSON object
-		const data = snapshot.val();
-
-		// Convert the JSON object to a string
-		const dataString = JSON.stringify(data, null, 2);
-
-		// Update the HTML content of the "shipment-data" element
-		const shipmentDataElement = document.getElementById('shipment-data');
-		shipmentDataElement.innerHTML = `<pre>${dataString}</pre>`;
-	});
-});
+// Get a reference to the "Shipments/Scanned" node in the Firebase database
+const scannedRef = firebase.database().ref('Shipments/Scanned');
 
 // Update the "Total" label with the total number of expected barcodes
 expectedRef.once('value').then(snapshot => {
-	const total = snapshot.numChildren();
-	document.getElementById('total').innerText = total;
+  const total = snapshot.numChildren();
+  document.getElementById('total').innerText = total;
 });
 
 // Update the "Scanned" label with the number of scanned barcodes
-const scannedRef = firebase.database().ref('Shipments/Scanned');
 scannedRef.on('value', snapshot => {
-	const scanned = snapshot.numChildren();
-	document.getElementById('scanned').innerText = scanned;
+  const scanned = snapshot.numChildren();
+  document.getElementById('scanned').innerText = scanned;
 });
